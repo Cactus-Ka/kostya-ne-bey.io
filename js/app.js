b848e36709842dadebe53ff8471ebc5d77d277d6
Cactus-Ka <126545362+Cactus-Ka@users.noreply.github.com>
2024-10-14 15:27:29 +0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,4 @@
+function toggleMenu() {
+  const navContainer = document.querySelector('.nav-container');
+  navContainer.style.display = navContainer.style.display === 'block' ? 'none' : 'block';
+}
